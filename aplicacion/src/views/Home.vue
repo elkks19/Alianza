a3377d2e07c4afe7b4d675162d5b8c66ae6ec186
elkks19 <rafafabiani1909@gmail.com>
2024-01-07 19:11:49 -0400

    prueba SSH

--- aplicacion/src/views/Home.vue
+++ aplicacion/src/views/Home.vue
@@ -1,3 +1,19 @@
 <template>
 	<h1>Home</h1>
+	<p v-for="a in this.prueba">{{a}}</p>
 </template>
+
+<script>
+export default{
+	data() {
+		return {
+			prueba: [
+				 'a',
+				 '2',
+				 'a',
+			]
+		}
+	}
+}
+</script>
+
